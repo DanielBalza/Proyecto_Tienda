2812923a80142b5132dbc2d2eb55b6d4c5bede27
DanielBalza <danielbalza1999@gmail.com>
2026-04-08 14:57:57 -0500

    Se añadio la logica del carrito e compra con javaScript y medidas de seguridad XSS

---
+++ script.js
@@ -0,0 +1,33 @@
+// 1. se definen variables 
+let cantidadCarrito = 0;
+let totalPrecio = 0;
+
+// 2. se definen funciones
+function añadirAlJuego(nombre, precio) {
+    // Sumamos 1 a la cantidad
+    cantidadCarrito++;
+
+    // Sumamos el precio al total
+    totalPrecio += precio;
+
+    // monstramos un mensaje al usuario
+    console.log(`Has añadido ${nombre} al carrito.`);
+
+    // Actualizamos la interfaz
+    actualizarInterfaz();
+
+    // validamos el total para que nunca sea valor negativo
+    if(totalPrecio < 0) {
+        totalPrecio = 0;
+        alert("Error en el calculo del precio.");
+    }
+}
+
+// 3. funcion para monstrar los cambios en la pantalla 
+function actualizarInterfaz() {
+    const contador =document.getElementById('contador-carrito');
+    const visualTotal = document.getElementById('total-precio');
+
+    if(contador) contador.innerText = cantidadCarrito;
+    if(visualTotal) visualTotal.innerText = totalPrecio.toFixed(2);
+}
